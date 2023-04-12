fa2e01f900c4c105ab4523dc9551677262e9c639
kavay1 <lucas.souzab@sempreceub.com>
2023-04-12 15:22:38 -0300

    solucao

--- algoritimos/algo5.go
+++ algoritimos/algo5.go
@@ -7,11 +7,18 @@ func main() { // Crie um Array bidimensional de inteiros com 3 linhas e 2 coluna
 	var matriz [3][2]int
 
 	for i :=  0; i < 3; i++ {
-		for j := 0; j < 2; i++ {
+		for j := 0; j < 2; j++ {
 			fmt.Printf("Digite os valores da linha e da coluna[%d][%d]: " ,i, j)
 			fmt.Scan(&matriz[i][j])
 		}
 	}
-	fmt.Println("Sua matriz ficou assim: ", matriz)
+	fmt.Println("Matriz resultante: ")
+
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 2; j++ {
+				fmt.Printf("[%d]", matriz[i][j])
+		}
+		fmt.Println()
+	}
 
 }
